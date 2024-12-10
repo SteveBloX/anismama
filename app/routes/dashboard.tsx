6ecb1e37a8b742167b0d18c640f07fa56e9cd088
SteveBloX <steve.block@protonmail.com>
2024-12-10 17:58:44 +0100

    quick dash

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -4,6 +4,7 @@ import { ROLE } from "~/types";
 import { getAllMangas } from "~/providers/lib";
 import { useLoaderData } from "@remix-run/react";
 import AnimatedStats from "~/components/stats";
+import { prisma } from "~/db.server";
 
 export const loader: LoaderFunction = async ({ request }) => {
   const user = await getUser(request);
@@ -14,23 +15,55 @@ export const loader: LoaderFunction = async ({ request }) => {
     return new Response("Forbidden", { status: 403 });
   }
   const allMangas = await getAllMangas();
-  return { mangasAmount: allMangas.length };
+  const users = await prisma.user.findMany();
+  const mangasCount = await prisma.manga.count();
+  const userMangaCount = await prisma.userManga.count();
+  return { mangasAmount: allMangas.length, users, mangasCount, userMangaCount };
 };
 
 export default function Dashboard() {
   const data: {
     mangasAmount: number;
+    users: { id: string; email: string }[];
+    mangasCount: number;
+    userMangaCount: number;
   } = useLoaderData();
   return (
     <div className="w-full px-4">
       <h1 className="text-3xl font-bold mb-3">Dashboard</h1>
-      <div className="flex flex-col justify-center gap-1 bg-gray-100 rounded-lg p-2 w-36">
-        <AnimatedStats
-          value={data.mangasAmount}
-          startDelay={0}
-          className="text-5xl flex justify-center"
-        />
-        <span className="text-center">{"mangas"}</span>
+      <div className="flex gap-4">
+        <div className="flex flex-col justify-center gap-1 bg-gray-100 rounded-lg p-2 w-36">
+          <AnimatedStats
+            value={data.mangasAmount}
+            startDelay={0}
+            className="text-5xl flex justify-center"
+          />
+          <span className="text-center">{"mangas"}</span>
+        </div>
+        <div className="flex flex-col justify-center gap-1 bg-gray-100 rounded-lg p-2 w-36">
+          <AnimatedStats
+            value={data.users.length}
+            startDelay={0}
+            className="text-5xl flex justify-center"
+          />
+          <span className="text-center">{"utilisateurs"}</span>
+        </div>
+        <div className="flex flex-col justify-center gap-1 bg-gray-100 rounded-lg p-2 w-36">
+          <AnimatedStats
+            value={data.mangasCount}
+            startDelay={0}
+            className="text-5xl flex justify-center"
+          />
+          <span className="text-center">{"mangas découverts"}</span>
+        </div>
+        <div className="flex flex-col justify-center gap-1 bg-gray-100 rounded-lg p-2 w-36">
+          <AnimatedStats
+            value={data.userMangaCount}
+            startDelay={0}
+            className="text-5xl flex justify-center"
+          />
+          <span className="text-center">{"usermangas"}</span>
+        </div>
       </div>
     </div>
   );
